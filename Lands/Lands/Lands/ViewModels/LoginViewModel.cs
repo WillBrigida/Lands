d82fd36c3a9222526a89232ae54d37c46b26acf8
will brigida <will_brigida@hotmail.com>
2018-03-18 01:58:10 -0300

    Atualização

---
+++ Lands/Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,59 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Lands.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool isRunning { get; set; }
+
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Ops!",
+                    "Você esqueceu de digitar seu Email",
+                    "Ok");
+                return;
+            }
+
+                if (string.IsNullOrEmpty(this.Password))
+                {
+                    await Application.Current.MainPage.DisplayAlert(
+                        "Ops!",
+                        "Você esqueceu de digitar sua senha",
+                        "Ok");
+                    return;
+                }
+
+                if (this.Email != "will_brigida@hotmail.com" || this.Password != "123456")
+                {
+                    await Application.Current.MainPage.DisplayAlert(
+                        "Ops!",
+                        "Login ou Senha inválido! Verifique os dados informados e tente novamente",
+                        "Ok");
+                    return;
+                }
+            
+        }
+
+        public LoginViewModel()
+        {
+
+        }
+    }
+}
--- Lands/Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/Lands/ViewModels/MainViewModel.cs
@@ -6,5 +6,14 @@ namespace Lands.ViewModels
 {
     public class MainViewModel
     {
+        public LoginViewModel Login {
+            get;
+            set;
+        }
+
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
     }
 }
